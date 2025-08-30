b69b06d3b839e738bbf323882783e8ef072fb4fa
Parvesh Balli <balliparvesh@gmail.com>
2025-08-31 00:21:16 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,4 @@
+print("Welcome To Band Generator")
+city = input("Which City You Have Born?\n ")
+petname = input("what is your pet name?\n ")
+print("Your Band Name Could Be " + city + " " + petname)
